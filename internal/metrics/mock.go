9ff09be976a5330c2a59d487966f79df067e4ec2
Atulya Singh <atulyarajsingh@gmail.com>
2026-03-16 19:40:42 -0400

    Created an interface for feeding data (fake data for now) and made 2 structs for podtemplates and nodetemplates

--- internal/metrics/mock.go
+++ internal/metrics/mock.go
@@ -1,1 +1,39 @@
 package metrics
+
+import (
+	"sync"
+
+	"github.com/atulya-singh/CourtVision/internal/types"
+)
+
+// Provider is the interface any metrics source must implement
+type Provider interface {
+	GetClusterSnapshot() (*types.ClusterSnapshot, error)
+}
+
+type MockProvider struct {
+	mu    sync.Mutex
+	tick  int
+	nodes []nodeTemplate
+	pods  []podTemplate
+}
+
+type nodeTemplate struct {
+	name     string
+	nodeType string
+	cpuCap   float64 //millicores
+	memCap   float64 //MB
+}
+
+type podTemplate struct {
+	name      string
+	namespace string
+	node      string
+	baseCPU   float64 //base CPU usage in millicores
+	baseMem   float64 // base mem usage in MB
+	cpuLimit  float64
+	cpuReq    float64
+	memLimit  float64
+	memReq    float64
+	noisy     bool // will this pode spike ?
+}
