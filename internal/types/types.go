64335bcd22ba9045183b8f01ec13efeaf76572ca
Atulya Singh <atulyarajsingh@gmail.com>
2026-03-15 23:55:27 -0400

    Created a struct to define actions that agent can do

---
+++ internal/types/types.go
@@ -0,0 +1,15 @@
+package types 
+
+import "time"
+
+//ActionType represents what the agent can do 
+type ActionType string
+
+const (
+	ActionNone ActionType = "none"
+	ActionPatchLimits ActionType = "patch_limits" // Rewrite resource limits on the fly
+	ActionEvictAndMove ActionType = "evict_and_move" //Move pod to a differnet node
+	ActionScaleDown ActionType = "scale_down" // Reduce replicas of the pod causing problems
+	ActionCordonNode ActionType = "cordon_node" // Mark node as unschedulable 
+)
+
