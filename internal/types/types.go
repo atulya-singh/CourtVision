79299419d1f6f1341561ee631e16e9a45de718b8
Atulya Singh <atulyarajsingh@gmail.com>
2026-03-16 00:01:20 -0400

    added a struct to classify the severity level of detected issue

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,15 +1,22 @@
-package types 
+package types
 
-import "time"
-
-//ActionType represents what the agent can do 
+// ActionType represents what the agent can do
 type ActionType string
 
 const (
-	ActionNone ActionType = "none"
-	ActionPatchLimits ActionType = "patch_limits" // Rewrite resource limits on the fly
+	ActionNone         ActionType = "none"
+	ActionPatchLimits  ActionType = "patch_limits"   // Rewrite resource limits on the fly
 	ActionEvictAndMove ActionType = "evict_and_move" //Move pod to a differnet node
-	ActionScaleDown ActionType = "scale_down" // Reduce replicas of the pod causing problems
-	ActionCordonNode ActionType = "cordon_node" // Mark node as unschedulable 
+	ActionScaleDown    ActionType = "scale_down"     // Reduce replicas of the pod causing problems
+	ActionCordonNode   ActionType = "cordon_node"    // Mark node as unschedulable
 )
 
+// Severity of the detected issue
+type Severity string
+
+const (
+	SeverityLow      Severity = "low"
+	SeverityMedium   Severity = "medium"
+	SeverityHigh     Severity = "high"
+	SeverityCritical Severity = "critical"
+)
