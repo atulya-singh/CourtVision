24fb015a6109f14a5ea9e4cab408fb42fd6080f1
Atulya Singh <atulyarajsingh@gmail.com>
2026-03-18 17:51:11 -0400

    initial commit for store.go

---
+++ internal/store/store.go
@@ -0,0 +1,14 @@
+package store
+
+import (
+	"sync"
+
+	"github.com/atulya-singh/CourtVision/internal/types"
+)
+
+type Store struct {
+	mu        sync.RWMutex
+	snapshot  types.ClusterSnapshot
+	decisions []types.Decision
+	listeners chan []types.Decision // sse subscribers
+}
