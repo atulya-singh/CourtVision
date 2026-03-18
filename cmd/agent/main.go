9635bba54915a4e4163c379f6ec4f4c3f227c500
Atulya Singh <atulyarajsingh@gmail.com>
2026-03-18 01:00:39 -0400

    Minor changes

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"github.com/atulya-singh/CourtVision/internal/metrics"
+	"github.com/atulya-singh/CourtVision/internal/types"
 )
 
 func main() {
@@ -15,4 +18,14 @@ func main() {
 	//constructors
 	provider := metrics.NewMockProvider()
 	engine := decision.newRuleBasedEngine()
+
+	//decision log
+	var allDecisions []types.Decision
+
+	// Main monitoring loop
+	interval := 5 * time.Second // slow interval
+	if len(os.Args) > 1 && os.Args[1] == "--fast" {
+		interval = 1 * time.Second // fast interval
+	}
+
 }
